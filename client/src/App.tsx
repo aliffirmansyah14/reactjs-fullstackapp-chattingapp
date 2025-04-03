6314d15eebf2048ad8546add7e1f2d271891c554
aliffirmansyah14 <aliffirmansyah1407@gmail.com>
2025-04-03 10:36:56 +0700

    add daisy Ui dev dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ export default function App() {
    return (
       <div className="max-w-xl mx-auto py-4">
          <h1 className="text-2xl">Hello world</h1>
+         <button className="btn btn-primary">Button</button>
       </div>
    );
 }
